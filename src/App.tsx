20d096e2064812fd3e739222892d42fe68cdb5dc
Maria Mizamidou <m.mizamidou@hotmail.com>
2024-11-28 13:00:46 +0000

    Changes to be made, leaderboard not displayed

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Map from './Components/Map';
 import Leaderboard from './Components/Leaderboard';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Map />} />
-				<Route path="/leaderboard" element={<Leaderboard />} />
-			</Routes>
-		</>
+		<Routes>
+			<Route path="/" element={<Map />} />
+			<Route path="/leaderboard" element={<Leaderboard />} />
+		</Routes>
 	);
 }
 
